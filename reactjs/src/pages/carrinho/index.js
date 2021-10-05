4349cd4a30a59346e16c01c73590a785dd7798e9
Gabriel <bielmelodossantos8@gmail.com>
2021-10-05 11:25:23 -0300

    ok

--- reactjs/src/pages/carrinho/index.js
+++ reactjs/src/pages/carrinho/index.js
@@ -1,5 +1,7 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Index() {
     return(
--- reactjs/src/pages/detalhe-produto/index.js
+++ reactjs/src/pages/detalhe-produto/index.js
@@ -1,5 +1,9 @@
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Index() {
     return(
